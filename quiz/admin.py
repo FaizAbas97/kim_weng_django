9fcebf2f7bc73e3a90dbde3e6b36dbb1f6818098
kimweng1980 <kimweng1980@gmail.com>
2026-01-26 22:17:30 +0800

    registering admin for quiz app

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from quiz.models import Student, Quiz, Question
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Quiz)
+admin.site.register(Question)
