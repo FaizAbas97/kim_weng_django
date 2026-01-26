ffb8502831a3664a289b33316566a972462583bc
root <root@ip-172-31-17-43.ap-southeast-1.compute.internal>
2026-01-26 12:48:04 +0000

    Initial commit from EC2

---
+++ quiz/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Quiz(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    date = models.CharField(max_length=50)
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+    is_true = models.BoeleanField()
