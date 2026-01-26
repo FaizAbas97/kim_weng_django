87d387aa1ae1ce132e97be55596b90ba9ef04efc
kimweng1980 <kimweng1980@gmail.com>
2026-01-26 23:15:34 +0800

    create new response message

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from quiz.models import Student
 # Create your views here.
 from django.http import HttpResponse
 
@@ -11,4 +11,10 @@ def index_2(request):
     return HttpResponse("Hello, world. You're at the quiz index part 2.")
 
 def index_3(request):
-    return HttpResponse("Welcome to index part 3.")   
+    students = Student.objects.all()
+    response_message = "Hi"
+    for student in students:
+        response_message = response_message + f"{student.first_name} {student.last_name}"
+    return HttpResponse(response_message)   
+
+
